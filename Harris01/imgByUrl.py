09922ac99d5710de9d694d5a1f8254f966d9d951
1924zjy0835 <1924083517@qq.com>
2020-04-29 18:51:40 +0800

    通过图片的url提取图片显示

--- Harris01/imgByUrl.py
+++ Harris01/imgByUrl.py
@@ -5,15 +5,15 @@
 # @CSDN: https://blog.csdn.net/zjy123078_zjy
 # @博客园: https://www.cnblogs.com/guyan-2020/
 
-# import numpy as np
-# import urllib.request as urllib
-# import cv2 as cv
-#
-# url = 'http://q87jey5py.bkt.clouddn.com/1586745385228.jpg'
-# resp = urllib.urlopen(url)
-# image = np.asarray(bytearray(resp.read()), dtype="uint8")
-# image = cv.imdecode(image, cv.IMREAD_COLOR)
-#
-# cv.imshow("Image", image)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+import numpy as np
+import urllib.request as urllib
+import cv2 as cv
+
+url = 'http://q87jey5py.bkt.clouddn.com/1586745385228.jpg'
+resp = urllib.urlopen(url)
+image = np.asarray(bytearray(resp.read()), dtype="uint8")
+image = cv.imdecode(image, cv.IMREAD_COLOR)
+
+cv.imshow("Image", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
